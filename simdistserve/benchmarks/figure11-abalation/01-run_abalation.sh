70c8291f5a2625550e39603629149851aeed1884
Ginda Chen <32371474+GindaChen@users.noreply.github.com>
2024-04-20 16:19:07 -0700

    Update 01-run_abalation.sh

--- simdistserve/benchmarks/figure11-abalation/01-run_abalation.sh
+++ simdistserve/benchmarks/figure11-abalation/01-run_abalation.sh
@@ -7,6 +7,7 @@ mkdir -p result
 config_tpl="--tp-prefill {tp_prefill} --pp-prefill {pp_prefill} --tp-decode {tp_decode} --pp-decode {pp_decode}"
 output_tpl_prod="--output {file_prefix}.latency.csv"
 output_tpl_debug="--output {file_prefix}.latency.csv --output-request-info  {file_prefix}.request_info.csv --output-request-event {file_prefix}.request_event.csv --output-request-latency {file_prefix}.request_latency.csv --output-worker {file_prefix}.worker.csv"
+exec_path=$(realpath ../simulate_dist.py)
 
 # if env has ABALATION_DEBUG=1, then use debug output
 if [ -z "$ABALATION_DEBUG" ]; then
@@ -17,7 +18,7 @@ else
 fi
 
 # Run experiment for DistServe
-client_cmdline="python3 ../simulate_dist.py --backend distserve --N {N} --workload {workload} --rate {rate} ${config_tpl} ${output_tpl}"
+client_cmdline="python3 ${exec_path} --backend distserve --N {N} --workload {workload} --rate {rate} ${config_tpl} ${output_tpl}"
 file_prefix='result/simulate-distserve-{workload}-n{N}-r{rate}-p{tp_prefill}{pp_prefill}{tp_decode}{pp_decode}'
 python ../simulate_multi.py \
 --client-cmdline "$client_cmdline" \
@@ -28,7 +29,7 @@ python ../simulate_multi.py \
 
 # Run experiment for vLLM
 # Note: `--xp-decode 0` is set becuase vLLM only use `--xp-prefill` as parallelism setting in the script.
-client_cmdline="python3 ../simulate_dist.py --backend vllm --N {N} --workload {workload} --rate {rate} ${config_tpl} ${output_tpl}"
+client_cmdline="python3 ${exec_path} --backend vllm --N {N} --workload {workload} --rate {rate} ${config_tpl} ${output_tpl}"
 file_prefix='result/simulate-vllm-{workload}-n{N}-r{rate}-p{tp_prefill}{pp_prefill}{tp_decode}{pp_decode}'
 python ../simulate_multi.py \
 --client-cmdline "$client_cmdline" \
