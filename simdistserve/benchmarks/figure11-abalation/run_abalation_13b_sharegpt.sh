567554fd37a56b7eae28804923865fea6204cedd
Ginda Chen <32371474+GindaChen@users.noreply.github.com>
2024-04-24 16:47:38 -0700

    Update run_abalation_13b_sharegpt.sh

--- simdistserve/benchmarks/figure11-abalation/run_abalation_13b_sharegpt.sh
+++ simdistserve/benchmarks/figure11-abalation/run_abalation_13b_sharegpt.sh
@@ -3,4 +3,4 @@
 workload=opt_13b_sharegpt
 sh 01-run_abalation.sh $workload
 mkdir -p arxiv-data/$workload
-cp -r figure result visual arxiv-data/$workload/
+mv figure result visual arxiv-data/$workload/
