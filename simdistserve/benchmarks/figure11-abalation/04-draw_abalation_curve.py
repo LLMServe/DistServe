1bcf1172b345bf6238ca5e4829be791dcfba75d3
Ginda Chen <32371474+GindaChen@users.noreply.github.com>
2024-04-23 21:24:19 -0700

    Update 04-draw_abalation_curve.py

--- simdistserve/benchmarks/figure11-abalation/04-draw_abalation_curve.py
+++ simdistserve/benchmarks/figure11-abalation/04-draw_abalation_curve.py
@@ -32,20 +32,20 @@ plt.ylabel(ylabel)
 ## SLO Scale
 plt.subplot(1, 2, 2)
 xlabel = "SLO Scale"
-SLO_scales = [1, 2, 3, 4, 5]
-with open("figure/figure_11a.json") as f:
+SLO_scales = [i * 0.2 + 0.2 for i in ([1, 2, 3, 4, 5])]
+with open("figure/figure_11b.json") as f:
     data = json.load(f)
     distllm_optimal_SLO_att = data['dist++']
     distllm_real_SLO_att = data['dist']
     vllm_plus_SLO_att = data['vllm++']
     vllm_SLO_att = data['vllm']
 
-plt.plot(rates, distllm_optimal_SLO_att, label='DistLLM-High', marker="o", markersize=markersize)
-plt.plot(rates, distllm_real_SLO_att, label='DistLLM-Low', marker="o", markersize=markersize)
-plt.plot(rates, vllm_plus_SLO_att, label='vLLM++', marker="o", markersize=markersize)
-plt.plot(rates, vllm_SLO_att, label='vLLM', marker="o", markersize=markersize)
-plt.plot([rates[0], rates[-1]], [att_target, att_target], '--')
-plt.xticks(rates, rates)
+plt.plot(SLO_scales, distllm_optimal_SLO_att, label='DistLLM-High', marker="o", markersize=markersize)
+plt.plot(SLO_scales, distllm_real_SLO_att, label='DistLLM-Low', marker="o", markersize=markersize)
+plt.plot(SLO_scales, vllm_plus_SLO_att, label='vLLM++', marker="o", markersize=markersize)
+plt.plot(SLO_scales, vllm_SLO_att, label='vLLM', marker="o", markersize=markersize)
+plt.plot([SLO_scales[0], SLO_scales[-1]], [att_target, att_target], '--')
+plt.xticks(SLO_scales, [f"{i:.1f}" for i in SLO_scales])
 plt.xlabel(xlabel)
 
 # plt.legend(frameon=False, bbox_to_anchor = (0.75, 1.3, 0, 0), ncol=2,
