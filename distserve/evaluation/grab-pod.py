3702ec938ac71ea1c66c71b4008ca56f2bf8b3db
interestingLSY <interestingLSY@gmail.com>
2024-04-25 10:50:18 +0800

    nits

--- distserve/evaluation/grab-pod.py
+++ distserve/evaluation/grab-pod.py
@@ -14,7 +14,8 @@ import time
 import random
 
 def request_pod(api_key: str, public_key: str, num_gpus: int):
-    data = '{"query": "mutation { podFindAndDeployOnDemand( input: { cloudType: SECURE, gpuCount: ' + str(num_gpus) + ', volumeInGb: 0, containerDiskInGb: 512, minVcpuCount: 32, minMemoryInGb: 512, gpuTypeId: \\"NVIDIA A100-SXM4-80GB\\", name: \\"distserve-evaluation\\", startJupyter: false, startSsh: true, templateId: \\"xbivg6n3b6\\", volumeKey: null, dockerArgs: \\"\\", ports: \\"8080/http,22/tcp,8000/tcp\\", dataCenterId: \\"US-OR-1\\", volumeMountPath: \\"/workspace\\", networkVolumeId:\\"dg0br51h50\\", env: [{ key: \\"PUBLIC_KEY\\", value: \\"' + public_key + '\\" }] } ) { id imageName env machineId machine { podHostId } } }"}'
+    data = '{"query": "mutation { podFindAndDeployOnDemand( input: { cloudType: SECURE, gpuCount: ' + str(num_gpus) + ', volumeInGb: 0, containerDiskInGb: 512, minVcpuCount: 32, minMemoryInGb: 128, gpuTypeId: \\"NVIDIA A100-SXM4-80GB\\", name: \\"distserve-evaluation\\", startJupyter: false, startSsh: true, templateId: \\"xbivg6n3b6\\", volumeKey: null, dockerArgs: \\"\\", ports: \\"8080/http,22/tcp,8000/tcp\\", dataCenterId: \\"US-OR-1\\", volumeMountPath: \\"/workspace\\", networkVolumeId:\\"dg0br51h50\\", env: [{ key: \\"PUBLIC_KEY\\", value: \\"' + public_key + '\\" }] } ) { id imageName env machineId machine { podHostId } } }"}'
+    print("Payload:", data)
     response = requests.post('https://api.runpod.io/graphql?api_key=' + api_key, headers={'content-type': 'application/json'}, data=data)
     print(response.text)
     json_data = response.json()
@@ -25,7 +26,9 @@ def request_pod(api_key: str, public_key: str, num_gpus: int):
             print("Alert! Unknown error: ", error)
             sys.exit(1)
     else:
-        assert "data" in json_data
+        if "data" not in json_data:
+            print("Error:", json_data)
+            return
         print("Pod deployed successfully")
         sys.exit(1)
 
@@ -38,5 +41,5 @@ if __name__ == "__main__":
     
     while True:
         request_pod(args.api_key, args.public_key, args.num_gpus)
-        time.sleep(1 + random.randint(-2, 5)/10)
+        time.sleep(1 + random.randint(-5, 5)/10)
     
