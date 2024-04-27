71124874230cf6083196588f1553657f1e2aef4e
Ginda Chen <32371474+GindaChen@users.noreply.github.com>
2024-04-26 17:39:40 -0700

    Update run_abalation.sh

--- simdistserve/benchmarks/figure11-abalation/run_abalation.sh
+++ simdistserve/benchmarks/figure11-abalation/run_abalation.sh
@@ -13,4 +13,4 @@ python 02-draw_rate_abalation.py --target "(400, 100)"
 python 03-draw_slo_abalation.py --target "(400, 100)" --per_gpu_rate 0.375
 python 04-draw_abalation_curve.py --rates "[0.0625, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75]"
 
-cp figure/abalation.png /workspace/
+cp figure/abalation.png figure/abalation.pdf /workspace/
