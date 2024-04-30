6956d175c3818422fab5d1953e5fe2815aad6871
interestingLSY <interestingLSY@gmail.com>
2024-04-30 14:08:38 +0800

    Add scripts for 175b model

--- distserve/evaluation/2-benchmark-serving/1-forge-175b-model.py
+++ distserve/evaluation/2-benchmark-serving/1-forge-175b-model.py
@@ -2,6 +2,6 @@ from transformers import PretrainedConfig, OPTForCausalLM, OPTConfig
 import torch
 
 torch.set_default_dtype(torch.float16)
-config = OPTConfig.from_pretrained("facebook/opt-13b")
+config = OPTConfig.from_pretrained("intlsy/opt-175b-hyperparam")
 model = OPTForCausalLM(config)
 model.save_pretrained("/dev/shm/forged-175b-model")
